4df7d5008c7b5e00dfc7d01e910bbf776be48931
deusbog <deusbogdan@gmail.com>
2023-01-20 22:05:24 +0300

    changed tab size

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-        let mut arr = [5, 4, 3, 2, 1];
-        arr.sort();
-        println!("{:?}", arr); // [1, 2, 3, 4, 5]
+    let mut arr = [5, 4, 3, 2, 1];
+    arr.sort();
+    println!("{:?}", arr); // [1, 2, 3, 4, 5]
 
 }
